0a994b7642410d8c39893d15e98cf8da709ebbdd
Lê Đức Cường <cuong.ld172990@sis.hust.edu.vn>
2020-04-20 10:01:27 +0700

    Update expand_api.py

--- expand_api.py
+++ expand_api.py
@@ -1,21 +1,23 @@
 from flask import request, Flask, jsonify
-from flask_cors import CORS
 from eval_teacher import expand
 from flask import jsonify
 import pickle
-
+import time
 
 expand("tôi là người ~ vn # .")
 
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/expand', methods=["POST"])
 def service():
     content = request.json['mytext']
-    result = expand(content)
-    return jsonify(result)
+    time_start = time.time()
+    result,score = expand(content)
+    time_pred = time.time() - time_start
+    out = {"expand":result,"score":score,"time":time_pred}
+    return jsonify(out)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port="5050")
